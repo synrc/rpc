be2648ed9aa6e4dc7f6ecd75dc1f9deb84364283
Namdak Tonpa <maxim@synrc.com>
2017-07-18 15:59:59 +0300

    Update Parser.swift

--- macbert/Source/Parser.swift
+++ macbert/Source/Parser.swift
@@ -106,102 +106,7 @@ class Tuple: Parsable {
                 if let atom = tuple.elements[0] as? BertAtom {
                     if atom.value == name! {
                         switch atom.value {
-                        case "Roster":
-                            if body.count != 10 {
-                                return nil
-                            }
-                            let roster = Roster()
-                            roster.id = body[0].parse(bert: tuple.elements[1]) as? Int
-                            roster.names = body[1].parse(bert: tuple.elements[2]) as? String
-                            roster.surnames = body[2].parse(bert: tuple.elements[3]) as? String
-                            roster.size = body[3].parse(bert: tuple.elements[4]) as? Int
-                            roster.userlist = body[4].parse(bert: tuple.elements[5]) as? [Contact]
-                            roster.roomlist = body[5].parse(bert: tuple.elements[6]) as? [Room]
-                            roster.subscribe = body[6].parse(bert: tuple.elements[7]) as? Bool
-                            roster.phone = body[7].parse(bert: tuple.elements[8]) as? String
-                            roster.avatar = body[8].parse(bert: tuple.elements[9]) as? String
-                            roster.status = body[9].parse(bert: tuple.elements[10]) as? String
-                            return roster
-                        case "Contact":
-                            if body.count != 6 {
-                                return nil
-                            }
-                            let contact = Contact()
-                            contact.phone_id = body[0].parse(bert: tuple.elements[1]) as? String
-                            contact.avatar = body[1].parse(bert: tuple.elements[2]) as? String
-                            contact.names = body[2].parse(bert: tuple.elements[3]) as? String
-                            contact.surnames = body[3].parse(bert: tuple.elements[4]) as? String
-                            contact.person_id = body[4].parse(bert: tuple.elements[5]) as? String
-                            contact.status = body[5].parse(bert: tuple.elements[6]) as? String
-                            return contact
-                        case "Profile":
-                            if body.count != 5 {
-                                return nil
-                            }
-                            let profile = Profile()
-                            profile.phone = body[0].parse(bert: tuple.elements[1]) as? String
-                            profile.data = body[1].parse(bert: tuple.elements[2]) as? String
-                            profile.person_id = body[2].parse(bert: tuple.elements[3]) as? String
-                            profile.accounts = body[3].parse(bert: tuple.elements[4]) as? [Int]
-                            profile.status = body[4].parse(bert: tuple.elements[5]) as? String
-                            return profile
-                        case "Room":
-                            if body.count != 4 {
-                                return nil
-                            }
-                            let room = Room()
-                            room.room = body[0].parse(bert: tuple.elements[1]) as? String
-                            room.description = body[1].parse(bert: tuple.elements[2]) as? String
-                            room.acl = body[2].parse(bert: tuple.elements[3]) as? [AnyObject]
-                            room.settings = body[3].parse(bert: tuple.elements[4]) as? [AnyObject]
-                            return room
-                        case "Join":
-                            if body.count != 4 {
-                                return nil
-                            }
-                            let join = Join()
-                            join.id = body[0].parse(bert: tuple.elements[1]) as? String
-                            join.user = body[1].parse(bert: tuple.elements[2]) as? String
-                            join.room = body[2].parse(bert: tuple.elements[3]) as? String
-                            join.answer = body[3].parse(bert: tuple.elements[4]) as? String
-                            return join
-                        case "Leave":
-                            if body.count != 4 {
-                                return nil
-                            }
-                            let leave = Leave()
-                            leave.id = body[0].parse(bert: tuple.elements[1]) as? String
-                            leave.user = body[1].parse(bert: tuple.elements[2]) as? String
-                            leave.room = body[2].parse(bert: tuple.elements[3]) as? String
-                            leave.answer = body[3].parse(bert: tuple.elements[4]) as? String
-                            return leave
-                        case "Approve":
-                            if body.count != 4 {
-                                return nil
-                            }
-                            let approve = Approve()
-                            approve.id = body[0].parse(bert: tuple.elements[1]) as? String
-                            approve.user = body[1].parse(bert: tuple.elements[2]) as? String
-                            approve.room = body[2].parse(bert: tuple.elements[3]) as? String
-                            approve.answer = body[3].parse(bert: tuple.elements[4]) as? String
-                            return approve
-                        case "Message":
-                            if body.count != 11 {
-                                return nil
-                            }
-                            let msg = Message()
-                            msg.msg_id = body[0].parse(bert: tuple.elements[1]) as? String
-                            msg.from = body[1].parse(bert: tuple.elements[2]) as? String
-                            msg.to = body[2].parse(bert: tuple.elements[3]) as? String
-                            msg.sync = (body[3].parse(bert: tuple.elements[4]) as? String) ?? ""
-                            msg.created = (body[4].parse(bert: tuple.elements[5]) as? String ) ?? ""
-                            msg.access = (body[5].parse(bert: tuple.elements[6]) as? String) ?? ""
-                            msg.started = (body[6].parse(bert: tuple.elements[7]) as? String) ?? ""
-                            msg.payload = body[7].parse(bert: tuple.elements[8]) as? String
-                            msg.mime = body[8].parse(bert: tuple.elements[9]) as? String
-                            msg.seen_by = body[9].parse(bert: tuple.elements[10]) as? String
-                            msg.status = body[10].parse(bert: tuple.elements[11]) as? String
-                            return msg
+                        
                         case "Err":
                             if body.count != 1 {
                                 return nil
@@ -224,21 +129,6 @@ class Tuple: Parsable {
                             io.code = body[0].parse(bert: tuple.elements[1]) as AnyObject
                             io.data = body[1].parse(bert: tuple.elements[2]) as AnyObject
                             return io
-                        case "Auth":
-                            if body.count != 9 {
-                                return nil
-                            }
-                            let auth = Auth()
-                            auth.token = body[0].parse(bert: tuple.elements[1]) as? String
-                            auth.dev_key = body[1].parse(bert: tuple.elements[2]) as? String
-                            auth.user_id = body[2].parse(bert: tuple.elements[3]) as? String
-                            auth.phone = body[3].parse(bert: tuple.elements[4]) as? Bool
-                            auth.client_id = body[4].parse(bert: tuple.elements[5]) as? String
-                            auth.type = body[5].parse(bert: tuple.elements[6]) as? String
-                            auth.sms_code = body[6].parse(bert: tuple.elements[7]) as? String
-                            auth.attempts = body[7].parse(bert: tuple.elements[8]) as? Int
-                            auth.services = body[8].parse(bert: tuple.elements[9]) as? [String]
-                            return auth
                         default:
                             return nil
                         }
