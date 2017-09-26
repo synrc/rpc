7226d4be74171adbe40a24dd15fddd9af1e7ef2c
Namdak Tonpa <maxim@synrc.com>
2017-09-27 01:02:50 +0300

    Swift/Java/JS

---
+++ src/macbert_java.erl
@@ -0,0 +1,12 @@
+-module(macbert_java).
+-export([parse_transform/2]).
+-compile(export_all).
+-include("io.hrl").
+
+parse_transform(Forms, _Options) ->
+    File = filename:join([?SRC,"java.java"]),
+    io:format("Generated Java: ~p~n",[File]),
+    file:write_file(File,directives(Forms)), Forms.
+directives(Forms) -> iolist_to_binary([ form(F) || F <- Forms ]).
+form({attribute,_,record,{List,T}}) -> [];
+form(Form) ->  [].
--- src/macbert_javascript.erl
+++ src/macbert_javascript.erl
@@ -3,21 +3,32 @@
 -compile(export_all).
 -include("io.hrl").
 
-parse_transform(Forms, _Options) -> file:write_file(?SRC++"/JSEnc/protocol.js",directives(Forms)), Forms.
+parse_transform(Forms, _Options) ->
+    File = filename:join([?SRC,"json-bert.js"]),
+    io:format("Generated JavaScript: ~p~n",[File]),
+    file:write_file(File,directives(Forms)), Forms.
 directives(Forms) -> iolist_to_binary([prelude(),xpack(),xunpack(),decode(Forms),encode(Forms),[ form(F) || F <- Forms ]]).
 form({attribute,_,record,{List,T}}) -> [encoder(List,T),decoder(List,T)];
 form(Form) ->  [].
-case_field({attribute,_,record,{List,T}},Prefix) -> lists:concat(["case '",List,"': ",Prefix,List,"(x)"]);
+case_field({attribute,_,record,{List,T}},Prefix) -> lists:concat(["case '",List,"': return ",Prefix,List,"(x); break"]);
 case_field(Form,_) ->  [].
 xpack()    -> "function pack(x)       { return x; }\n".
 xunpack()  -> "function unpack(x)     { return x; }\n".
 prelude()  -> "function str2ab(str)   { return new TextEncoder('utf-8').encode(str).buffer; };\n"
+              "function clean(r)      { for(var k in r) if(!r[k]) delete r[k]; return r; }\n"
               "function type(data)    {\n"
-              "    var res = '';\n"
+              "    var res = undefined;\n"
               "    switch (typeof data) {\n" % TODO: This is not needed, we can handle it in Erlang
               "        case 'string': case 'number': res = data; break;\n"
               "        case 'object': res = utf8_dec(data); break;\n"
               "        case 'undefined': res = ''; break;\n"
+              "        default: console.log('Strange data: ' + data); }\n"
+              "    return res; };\n"
+              "function scalar(data)    {\n"
+              "    var res = undefined;\n"
+              "    switch (typeof data) {\n" % TODO: This is not needed, we can handle it in Erlang
+              "        case 'string': res = bin(data); break; case 'number': res = number(data); break;\n"
+              "        case 'object': res = encode(data); break;\n"
               "        default: console.log('Strange data: ' + data); }\n"
               "    return res; };\n"
               "function enc_97(Data)  { return {t: 97,  v: Number(Data)}; };\n"
@@ -26,8 +37,8 @@ prelude()  -> "function str2ab(str)   { return new TextEncoder('utf-8').encode(s
               "function enc_108(Data) { return {t: 108, v: Data}; };\n"
               "function enc_109(Data) { return {t: 109, v: str2ab(Data)}; };\n"
               "function enc_110(Data) { return {t: 110, v: Number(Data)}; };\n\n".
-decode(F) -> lists:concat(["function decode(x) {\n    switch (x.v[0].v) {\n\t",case_fields(F,"dec"),";\n\tdefault: {}\n}\n\n"]).
-encode(F) -> lists:concat(["function encode(x) {\n    switch (x.tupleName) {\n\t",case_fields(F,"enc"),";\n\tdefault: {}\n}\n\n"]).
+decode(F) -> lists:concat(["function decode(x) {\n if (x.t == 104) { switch (x.v[0].v) {\n\t",case_fields(F,"dec"),";\n\tdefault: return x.v;\n    } } else return x.v; \n}\n\n"]).
+encode(F) -> lists:concat(["function encode(x) {\n switch (x.tupleName) {\n\t",case_fields(F,"enc"),";\n\tdefault: return scalar(x);\n    }\n}\n\n"]).
 case_fields(Forms,Prefix) -> string:join([ case_field(F,Prefix) || F <- Forms, case_field(F,Prefix) /= []],";\n\t").
 
 decoder(List,T) ->
@@ -35,18 +46,18 @@ decoder(List,T) ->
    Fields =  [{ lists:concat([Field]), {Name,Args}}
           || {_,{_,_,{atom,_,Field},Value},{type,_,Name,Args}} <- T ],
    case Fields of [] -> []; _ ->
-   iolist_to_binary(["function dec",L,"(d) {\n    var r={};\n\t",
-     "r.tupleName = '",L,"';\n\t", string:join([ dispatch_dec(Type,Name,I) ||
-     {{Name,Type},I} <- lists:zip(Fields,lists:seq(1,length(Fields))) ],",\n\t"),
-     ";\n    return r; }\n\n"]) end.
+   iolist_to_binary(["function dec",L,"(d) {\n    var r={}; ",
+     "r.tupleName = '",L,"';\n    ", string:join([ dispatch_dec(Type,Name,I) ||
+     {{Name,Type},I} <- lists:zip(Fields,lists:seq(1,length(Fields))) ],";\n    "),
+     ";\n    return clean(r); }\n\n"]) end.
 
 encoder(List,T) ->
    L = nitro:to_list(List),
    Fields =  [{ lists:concat([Field]), {Name,Args}}
           || {_,{_,_,{atom,_,Field},Value},{type,_,Name,Args}} <- T ],
    case Fields of [] -> []; _ ->
-   iolist_to_binary(["function enc",L,"(d) {\n    var tupleName = atom('",L,"'),\n\t",
-     string:join([ dispatch_enc(Type,Name) || {Name,Type} <- Fields ],",\n\t"),
+   iolist_to_binary(["function enc",L,"(d) {\n    var tupleName = atom('",L,"');\n    ",
+     string:join([ dispatch_enc(Type,Name) || {Name,Type} <- Fields ],";\n    "),
      ";\n    return tuple(tupleName,",string:join(element(1,lists:unzip(Fields)),","),"); }\n\n"]) end.
 
 pack({Name,{tuple,_}})    -> lists:concat(["encode(d.",Name,")"]);
@@ -56,34 +67,40 @@ pack({Name,{list,[]}})    -> lists:concat(["list(d.",Name,")"]);
 pack({Name,{atom,[]}})    -> lists:concat(["atom(d.",Name,")"]);
 pack({Name,{binary,[]}})  -> lists:concat(["bin(d.",Name,")"]);
 pack({Name,{union,[{type,_,nil,[]},{type,_,Type,Args}]}}) -> pack({Name,{Type,Args}});
-pack({Name,Args}) -> io:format("name:~p~nargs:~p~n",[Name,Args]), io_lib:format("'TODO:~w'",[Args]).
+pack({Name,{union,[{type,_,nil,[]},{atom,_,_}|_]}}) -> lists:concat(["atom(d.",Name,")"]);
+pack({Name,Args}) ->
+    n2o:info(?MODULE,"pack:~p~nargs:~p~n",[Name,Args]),
+    io_lib:format("bin(d.~s)",[Name]).
 
-unpack({Name,{tuple,_}},I)    -> lists:concat(["decode(d[",I,"].v)"]);
-unpack({Name,{term,_}},I)     -> lists:concat(["decode(d[",I,"].v)"]);
-unpack({Name,{integer,[]}},I) -> lists:concat(["type(d[",I,"].v)"]);
-unpack({Name,{atom,[]}},I)    -> lists:concat(["type(d[",I,"].v)"]);
-unpack({Name,{list,[]}},I)    -> lists:concat(["type(d[",I,"].v)"]);
-unpack({Name,{binary,[]}},I)  -> lists:concat(["type(d[",I,"].v)"]);
+unpack({Name,{tuple,_}},I)    -> lists:concat(["decode(d.v[",I,"].v)"]);
+unpack({Name,{term,_}},I)     -> lists:concat(["decode(d.v[",I,"].v)"]);
+unpack({Name,{integer,[]}},I) -> lists:concat(["type(d.v[",I,"].v)"]);
+unpack({Name,{atom,[]}},I)    -> lists:concat(["type(d.v[",I,"].v)"]);
+unpack({Name,{list,[]}},I)    -> lists:concat(["type(d.v[",I,"].v)"]);
+unpack({Name,{binary,[]}},I)  -> lists:concat(["type(d.v[",I,"].v)"]);
 unpack({Name,{union,[{type,_,nil,[]},{type,_,Type,Args}]}},I) -> unpack({Name,{Type,Args}},I);
-unpack({Name,Args},I) -> io:format("name:~p~nargs:~p~n",[Name,Args]), io_lib:format("'TODO:~w'",[Args]).
+unpack({Name,Args},I) ->
+    n2o:info(?MODULE,"unpack:~p~nargs:~p~n",[Name,Args]),
+    io_lib:format("bin(\"TODO:~w\")",[Args]),
+    lists:concat(["type(d.v[",I,"].v)"]).
 
 dispatch_dec({union,[{type,_,nil,[]},{type,_,list,Args}]},Name,I) -> dispatch_dec({list,Args},Name,I);
-dispatch_dec({list,[{type,_,record,[{atom,_,Class}]}]},Name,I) -> dec_list(Name,lists:concat([Class]),integer_to_list(I));
+dispatch_dec({list,_},Name,I) -> dec_list(Name,integer_to_list(I));
 dispatch_dec(Type,Name,I) ->
-    lists:concat(["r.",Name," = d && d.v[",I,"] ? ",unpack({Name,Type},integer_to_list(I))," : '';"]).
+    lists:concat(["r.",Name," = d && d.v[",I,"] ? ",unpack({Name,Type},integer_to_list(I))," : undefined"]).
 
 dispatch_enc({union,[{type,_,nil,[]},{type,_,list,Args}]},Name) -> dispatch_enc({list,Args},Name);
-dispatch_enc({list,[{type,_,record,[{atom,_,Class}]}]},Name) -> enc_list(Name,lists:concat([Class]));
+dispatch_enc({list,_},Name) -> enc_list(Name);
 dispatch_enc(Type,Name) ->
-    lists:concat([Name," = '",Name,"' in d && d.",Name," ? ",pack({Name,Type})," : enc_106()"]).
+    lists:concat(["var ", Name," = '",Name,"' in d && d.",Name," ? ",pack({Name,Type})," : enc_106()"]).
 
-enc_list(Name,Class) ->
+enc_list(Name) ->
     lists:flatten([
-    "'",Name,"' in d && d.",Name," ?"
-    " (d.",Name,".forEach(function(x){",Name,".push(enc",Class,"(x))}),", Name,"=enc_108(",Name,"))"
-    " : (",Name," = enc_106())"]).
+    "var ",Name," = []; if ('",Name,"' in d && d.",Name,") {"
+    " d.",Name,".forEach(function(x){",Name,".push(encode(x))}); ", Name,"=enc_108(",Name,"); } else"
+    " { ",Name," = enc_106() }"]).
 
-dec_list(Name,Class,I) ->
-    lists:flatten(["(d && d.v[",I,"] && d.v[",I,"].v) ?",
-        " d.v[",I,"].v.forEach(function(x){r.",Name,".push(dec",Class,"(x.v))}) :",
-        " r.",Name," = ''"]).
+dec_list(Name,I) ->
+    lists:flatten(["r.",Name," = []; (d && d.v[",I,"] && d.v[",I,"].v) ?",
+        " d.v[",I,"].v.forEach(function(x){r.",Name,".push(decode(x))}) :",
+        " r.",Name," = undefined"]).
