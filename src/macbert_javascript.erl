befb610bc33b08d0dae323903b6eca0937d4b6c8
Namdak Tonpa <maxim@synrc.com>
2017-09-26 21:49:37 +0300

    handle unions

--- src/macbert_javascript.erl
+++ src/macbert_javascript.erl
@@ -4,12 +4,13 @@
 -include("io.hrl").
 
 parse_transform(Forms, _Options) -> file:write_file(?SRC++"/JSEnc/protocol.js",directives(Forms)), Forms.
-directives(Forms) -> iolist_to_binary([prelude(),pack(),unpack(),[ form(F) || F <- Forms ]]).
+directives(Forms) -> iolist_to_binary([prelude(),xpack(),xunpack(),[ form(F) || F <- Forms ]]).
 form({attribute,_,record,{List,T}}) -> [encoder(List,T),decoder(List,T)];
 form(Form) ->  [].
-pack()     -> "function pack(x) { return x; }\n".
-unpack()   -> "function unpack(x) { return x; }\n".
-prelude()  -> "function enc_97(Data)  { return {t: 97,  v: Number(Data)}; };\n"
+xpack()    -> "function pack(x)       { return x; }\n".
+xunpack()  -> "function unpack(x)     { return x; }\n".
+prelude()  -> "function str2ab(str)   { return new TextEncoder('utf-8').encode(str).buffer; };\n"
+              "function enc_97(Data)  { return {t: 97,  v: Number(Data)}; };\n"
               "function enc_100(Data) { return {t: 100, v: Data}; };\n"
               "function enc_106()     { return {t: 106, v: undefined}; };\n"
               "function enc_108(Data) { return {t: 108, v: Data}; };\n"
@@ -35,15 +36,25 @@ encoder(List,T) ->
      string:join([ dispatch_enc(Type,Name) || {Name,Type} <- Fields ],",\n\t"),
      ";\n    return tuple(tupleName,",string:join(element(1,lists:unzip(Fields)),","),"); }\n\n"]) end.
 
-type({Name,Args}) -> io:format("name:~p~nargs:~p~n",[Name,Args]), io_lib:format("'~w'",[{Name,Args}]).
+pack({Name,{union,[{type,_,nil,[]},{type,_,integer,[]}]}}) -> lists:concat(["number(d.",Name,")"]);
+pack({Name,{union,[{type,_,nil,[]},{type,_,atom,[]}]}})    -> lists:concat(["atom(d.",Name,")"]);
+pack({Name,{union,[{type,_,nil,[]},{type,_,binary,[]}]}})  -> lists:concat(["bin(d.",Name,")"]);
+pack({Name,Args}) -> io:format("name:~p~nargs:~p~n",[Name,Args]), io_lib:format("'~w'",[Args]).
 
+unpack({Name,{union,[{type,_,nil,[]},{type,_,integer,[]}]}},I) -> lists:concat(["type(d[",I,"].v)"]);
+unpack({Name,{union,[{type,_,nil,[]},{type,_,atom,[]}]}},I)    -> lists:concat(["type(d[",I,"].v)"]);
+unpack({Name,{union,[{type,_,nil,[]},{type,_,binary,[]}]}},I)  -> lists:concat(["type(d[",I,"].v)"]);
+unpack({Name,Args},I) -> io:format("name:~p~nargs:~p~n",[Name,Args]), io_lib:format("'~w'",[Args]).
+
+dispatch_dec({union,[{type,_,nil,[]},{type,_,list,Args}]},Name,I) -> dispatch_dec({list,Args},Name,I);
 dispatch_dec({list,[{type,_,record,[{atom,_,Class}]}]},Name,I) -> dec_list(Name,lists:concat([Class]),integer_to_list(I));
 dispatch_dec(Type,Name,I) ->
-    lists:concat(["r.",Name," = d && d.v[",I,"] ? unpack(",type(Type),",d.v[",I,"].v) : '';"]).
+    lists:concat(["r.",Name," = d && d.v[",I,"] ? ",unpack({Name,Type},integer_to_list(I))," : '';"]).
 
+dispatch_enc({union,[{type,_,nil,[]},{type,_,list,Args}]},Name) -> dispatch_enc({list,Args},Name);
 dispatch_enc({list,[{type,_,record,[{atom,_,Class}]}]},Name) -> enc_list(Name,lists:concat([Class]));
 dispatch_enc(Type,Name) ->
-    lists:concat([Name," = '",Name,"' in d && d.",Name," ? pack(",type(Type),",d.",Name,") : enc_106()"]).
+    lists:concat([Name," = '",Name,"' in d && d.",Name," ? ",pack({Name,Type})," : enc_106()"]).
 
 enc_list(Name,Class) ->
     lists:flatten([
