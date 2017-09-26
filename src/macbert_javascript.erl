5ae2a07eec6b61e295df3b73084e49b3dc917eb6
Namdak Tonpa <maxim@synrc.com>
2017-09-26 20:48:43 +0300

    js progress

--- src/macbert_javascript.erl
+++ src/macbert_javascript.erl
@@ -3,36 +3,40 @@
 -compile(export_all).
 -include("io.hrl").
 
-parse_transform(Forms, _Options) -> directives(Forms), Forms.
-directives(Forms) -> lists:flatten([ form(F) || F <- Forms ]).
-
-form({attribute,_,record,{List,T}})  ->
-   [X|Rest]=atom_to_list(List),
-   case X >= $A andalso X =< $Z andalso List /= 'Client'
-                                orelse List == io
-                                orelse List == p2p
-                                orelse List == error
-                                orelse List == push
-                                orelse List == ok2
-                                orelse List == error2
-                                orelse List == ok of true
-      -> encoder(List,T),
-         decoder(List,T),
-         {List,T};
-    _ -> [] end;
+parse_transform(Forms, _Options) -> file:write_file(?SRC++"/JSEnc/protocol.js",directives(Forms)), Forms.
+directives(Forms) -> iolist_to_binary([prelude(),pack(),unpack(),[ form(F) || F <- Forms ]]).
+form({attribute,_,record,{List,T}}) -> [encoder(List,T),decoder(List,T)];
 form(Form) ->  [].
+pack()     -> "function pack(x) { return x; }\n".
+unpack()   -> "function unpack(x) { return x; }\n".
+prelude()  ->
+  "var enc_97 = function (Data) { return {t: 97, v: Number(Data)}; };\n"
+  "var enc_100 = function (Data) { return {t: 100, v: Data}; };\n"
+  "var enc_106 = function () { return {t: 106, v: undefined}; };\n"
+  "var enc_108 = function (Data) { return {t: 108, v: Data}; };\n"
+  "var enc_109 = function (Data) { return {t: 109, v: str2ab(Data)}; };\n"
+  "var enc_110 = function (Data) { return {t: 110, v: Number(Data)}; };\n\n".
 
 decoder(List,T) ->
-    [].
+   L = nitro:to_list(List),
+   Fields =  [{ lists:concat([Field]), {Name,Args}}
+          || {_,{_,_,{atom,_,Field},Value},{type,_,Name,Args}} <- T ],
+   case Fields of [] -> []; _ ->
+   iolist_to_binary(["function dec",L,"(d) {\n    var r={};\n\t",
+     "r.tupleName = '",L,"';\n\t",
+     string:join([lists:concat(["r.",Name," = d && d.v[",I,"] ? unpack(",type(Type),",d.v[",I,"].v) : '';"])
+        || {{Name,Type},I} <- lists:zip(Fields,lists:seq(1,length(Fields))) ],"\n\t"),
+     ";\n    return r; }\n\n"]) end.
 
 encoder(List,T) ->
    L = nitro:to_list(List),
-   Fields =  [{ lists:concat([Field]), io_lib:format("~p(~s)",[bin,Name])}
+   Fields =  [{ lists:concat([Field]), {Name,Args}}
           || {_,{_,_,{atom,_,Field},Value},{type,_,Name,Args}} <- T ],
-   file:write_file(?SRC++"/JSEnc/"++L++".js",
-   iolist_to_binary(["\nps.",L," = function(Data) { var ",
+   case Fields of [] -> []; _ ->
+   iolist_to_binary(["function enc",L,"(d) {\n    var tupleName = atom('",L,"'),\n\t",
      string:join([
-     lists:concat([Name," = '",Name,"' in Data ? ",Type," : []"]) ||
-     {Name,Type} <- Fields ],",\n\t"),
-     ";\n  return enc(tuple(atom(",L,"),",string:join(element(1,lists:unzip(Fields)),","),")); }"])).
+     lists:concat([Name," = '",Name,"' in d && d.",Name," ? pack(",type(Type),",d.",Name,") : enc_106()"])
+          || {Name,Type} <- Fields ],",\n\t"),
+     ";\n    return tuple(tupleName,",string:join(element(1,lists:unzip(Fields)),","),"); }\n\n"]) end.
 
+type({Name,Args}) -> io:format("name:~p~nargs:~p~n",[Name,Args]), io_lib:format("'~w'",[{Name,Args}]).
