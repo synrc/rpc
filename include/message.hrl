672a2a27f0246954f3419099e89b6bb25a4ebbe1
Dmitro Boiko <boo.bda88@gmail.com>
2018-10-25 17:32:35 +0300

    add rpc functions. Add friend module.

--- include/message.hrl
+++ include/message.hrl
@@ -4,6 +4,7 @@
 -type container()     :: chain | cur.
 -type messageType()   :: sys | reply | forward | read | edited | cursor.
 -type messageStatus() :: masync | mdelete | mclear| mupdate | medit.
+-type historyType()   :: updated | get | update | last_loaded | last_msg | get_reply.
 
 -record(muc,            {name = [] :: [] | binary() }).
 -record(p2p,            {from = [] :: [] | binary(),
@@ -25,5 +26,12 @@
                          repliedby = [] :: list(integer()),
                          mentioned = [] :: list(integer()),
                          mstatus   = [] :: messageStatus()}).
+
+-record('History',      {roster_id = [] :: [] | binary(),
+                         feed      = [] :: [] | #p2p{} | #muc{},
+                         size      = 0  :: [] | integer(),
+                         entity_id = 0  :: [] | integer(),
+                         data      = [] :: integer(),
+                         status    = [] :: historyType()}).
 
 -endif.
---
+++ src/services/bert_friend.erl
@@ -0,0 +1,3 @@
+-module(bert_friend).
+-include_lib("bert/include/feature.hrl").
+-compile({parse_transform, bert_google}).
