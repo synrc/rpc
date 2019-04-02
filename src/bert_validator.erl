6d01890639016c5ff78d04c723a07a8a1b101ebf
cryoflamer <cryoflamer22@gmail.com>
2019-04-02 18:02:56 +0300

    fix bert_validator

--- src/bert_validator.erl
+++ src/bert_validator.erl
@@ -6,6 +6,11 @@
 
 -record(form, {validators = [], module = [], files = [], types = [], name = [], record = [], fields = []}).
 
+-define(AND(L), case L of [] -> ""; _ -> [" andalso "] end).
+-define(OR(L), case L of [] -> "";_ -> [" orelse "] end).
+-define(LB(L), case L of [] -> ""; _ -> ["("] end).
+-define(RB(L), case L of [] -> ""; _ -> [")"] end).
+
 -define(Valid_Start,     "ErrFields = lists:foldl(fun ({RecField, F}, Acc2) ->
     case {RecField, F} of\n\t\t").
 -define(Valid_fun(Name), "\n\t\t_ -> [{RecField, D}|Acc2]
@@ -83,83 +88,75 @@ valid([],#form{record = Class}, Acc) ->
     case W of [] -> [];
         _ -> "\nvalidate(D = #'" ++ Class ++ "'{" ++ M ++ "}, Acc) -> \n\t" ++ ?Valid_Start ++ W ++ ?Valid_fun(Class) end;
 valid([{Name, Type} | Rest], #form{} = Form, Acc) ->
-  valid(Rest, Form, Acc++[get_data(Type, Form, atom_to_list(Name))]);
+  valid(Rest, Form, Acc++[{get_fields(atom_to_list(Name), Type), get_type(Type, u(atom_to_list(Name)), Form)}]);
 valid([_ | _Rest], _Form, _Acc) -> [].
 
-get_data(Type,#form{record = Class}, Name)  ->  {get_fields(Name, Type), get_type(Type, u(Name), Class)}.
+get_data(Type, Form, Name)  ->  {get_fields(Name, Type), get_type(Type, u(Name), Form)}.
 
-get_type({integer,_},Name,_)                                -> {"{" ++ l(Name) ++ ",_} when is_integer("++Name++") -> Acc2;",[]};
-get_type({list,[{type,_,record,[{atom,_,C}]}]} = L,Name,_)  -> {guard(L, Name, "{" ++ l(Name) ++ ",_} "++["when "]),{Name,C}};
-get_type({list,[{type,_,union, _}]} = L,Name,_)             -> {guard(L, Name, "{" ++ l(Name) ++ ",_} "++["when "]),Name};
-get_type({list,_}=L,Name,_)                                 -> {guard(L, Name, "{" ++ l(Name) ++ ",_} "++["when "]),[]};
-get_type({record,[{atom,_,Atom}]} = R,Name,_Class)          -> {guard(R, Name, "{" ++ l(Name)),[]};%% TODO validate inner record
-get_type({term,[]},Name,_)                                  -> {"{" ++ l(Name)++",_} -> Acc2;",[]};
-get_type({union,R},Name,Class) when is_list(R)              -> {guard({union, R}, Name, "{"++ l(Name)++ ",_} "++["when "]), []};
-get_type({tuple,_},Name,_)                                  -> {"{" ++ l(Name) ++ ",_} when is_tuple("++Name++") -> Acc2;",[]};
-get_type({atom,_},Name,_)                                   -> {"{" ++ l(Name) ++ ",_} when is_atom("++Name++") -> Acc2;",[]};
-get_type({binary,_},Name,_)                                 -> {"{" ++ l(Name) ++ ",_} when is_binary("++Name++") -> Acc2;",[]};
-get_type(atom,Name,_)                                       -> {"{" ++ l(Name) ++ ",_} when is_atom("++Name++") -> Acc2;",[]};
-get_type({atom, _, Default},Name,_)                         -> {"{" ++ l(Name) ++ ","++atom_to_list(Default)++"} -> Acc2;",[]};
-get_type(integer,Name,_)                                    -> {"{" ++ l(Name) ++ ",_} when is_integer("++Name++") -> Acc2;",[]};
-get_type(Type,Name,_)                                       -> get_records(Type,Name).
+get_fields(Name, Type) ->
+    lists:concat([Name, " = ", case Type of binary -> "<<_/binary>>"; _ -> u(Name) end]).
 
--define(AND(L), case L of [] -> ""; _ -> [" andalso "] end).
--define(OR(L), case L of [] -> "";_ -> [" orelse "] end).
--define(LB(L), case L of [] -> ""; _ -> ["("] end).
--define(RB(L), case L of [] -> ""; _ -> [")"] end).
+get_type({integer,_}, Name, _Form)                      -> {"{" ++ l(Name) ++ ",_} when is_integer("++Name++") -> Acc2;",[]};
+get_type({list,_} = L, Name, Form)                  -> {guard(L, Name, Form, "{" ++ l(Name) ++ ",_} "++["when "]),[]};
+get_type({record,[{atom,_,_Atom}]} = R, Name, Form) -> {guard(R, Name, Form, "{" ++ l(Name)),[]};%% TODO validate inner record
+get_type({term,[]}, Name, _Form)                    -> {"{" ++ l(Name)++",_} -> Acc2;",[]};
+get_type({union,R}, Name, Form)                     -> {guard({union, R}, Name, Form, "{"++ l(Name)++ ",_} "++["when "]), []};
+get_type({tuple,_}, Name, _Form)                    -> {"{" ++ l(Name) ++ ",_} when is_tuple("++Name++") -> Acc2;",[]};
+get_type({atom,_}, Name, _Form)                     -> {"{" ++ l(Name) ++ ",_} when is_atom("++Name++") -> Acc2;",[]};
+get_type({binary,_}, Name, _Form)                   -> {"{" ++ l(Name) ++ ",_} when is_binary("++Name++") -> Acc2;",[]};
+get_type(atom,Name, _Form)                          -> {"{" ++ l(Name) ++ ",_} when is_atom("++Name++") -> Acc2;",[]};
+get_type({atom, _, Atom},Name, _Form)               -> {"{" ++ l(Name) ++ ","++atom_to_list(Atom)++"} -> Acc2;",[]};
+get_type(integer, Name, _Form)                      -> {"{" ++ l(Name) ++ ",_} when is_integer("++Name++") -> Acc2;",[]};
+get_type({type,_,record,[{_,_,Class}]},Name, _Form) -> {atom_to_list(Class),"is_record("++Name++",'"++atom_to_list(Class)++"')"};
+get_type({type,_,binary,_},Name, _Form)             -> {[],"is_binary("++Name++")"};
+get_type({type,_,integer,_},Name, _Form)            -> {[],"is_integer("++Name++")"};
+get_type({type,_,nil,_},Name, _Form)                -> {[],Name++"==[]"};
+get_type({type,_,tuple,_},Name, _Form)              -> {[],"is_tuple("++Name++")"};
+get_type({type,_,list,_},Name, _Form)               -> {[],"is_list("++Name++")"};
+get_type({type,_,atom,_},Name, _Form)               -> {[],"is_atom("++Name++")"};
+%%get_type({atom,_,V},Name, _Form)                    -> {[],Name++"=='" ++ atom_to_list(V)++"'"};
+get_type(_,_, _Form)                                -> {[],[]}.
 
-guard({term,[]},_Name,Acc) -> Acc++",_} -> Acc2;";
-guard({record,[{atom,_,Atom}]},_Name,Acc) -> Acc++", #'"++atom_to_list(Atom)++"'{}} -> Acc2;";
-guard({list, [{type, _, union, []}]}, _Name, Acc) -> Acc;
-guard({list, [{type, _, union, L} = T]}, Name, Acc) when is_list(L) ->
-    Acc2 = guard(T, "Tmp", Acc ++"is_list("++Name++") ->\n\t\t\tlists:foldl(fun(Tmp, Acc3)"++[" when "]),
+guard({term,[]},_Name, _Form, Acc) -> Acc++",_} -> Acc2;";
+guard({record,[{atom,_,Atom}]},_Name, _Form, Acc) -> Acc++", #'"++atom_to_list(Atom)++"'{}} -> Acc2;";
+guard({list, [{type, _, union, []}]}, _Name, _Form, Acc) -> Acc;
+guard({list, [{type, _, union, L} = T]}, Name, Form,Acc) when is_list(L) ->
+    Acc2 = guard(T, "Tmp", Form, Acc ++"is_list("++Name++") ->\n\t\t\tlists:foldl(fun(Tmp, Acc3)"++[" when "]),
     case lists:last(Acc2) of
         " -> Acc2;" -> lists:droplast(Acc2)++" -> validate(Tmp, Acc3); (Tmp, Acc3) -> [{"++l(Name)++", D}|Acc3] end, Acc2, "++Name++");";
         _-> lists:flatten(Acc) end;
-guard({list, [{type, I, N, R}]}, Name, Acc) ->
-    guard({list, [{type, I, union, [{type, I, N, R}]}]}, Name, Acc);
-guard({list, _}, Name, Acc) -> Acc++"is_list("++Name++") -> [];";
-guard({type, _, union, U}, Name, Acc) -> guard({union, U}, Name, Acc);
-guard({union, []}, _Name, Acc) -> Acc++[" -> Acc2;"];
-guard({union, [{atom, _, A}|T]}, Name, Acc) ->
-    guard({union, T}, Name, Acc++Name++"=='"++atom_to_list(A)++"'"++?OR(T));
-guard({union, [{type, _, nil, []}|T]}, Name, Acc) ->
-    guard({union, T}, Name, Acc++Name++"==[]"++?OR(T));
-guard({union, [{type, _, tuple, _}|T]}, Name, Acc) ->
-    guard({union, T}, Name, Acc++"is_tuple("++Name++")"++?OR(T));
-guard({union, [{type, _, iolist, _}|T]}, Name, Acc) ->
-    guard({union, T}, Name, Acc++"is_list("++Name++")"++?OR(T));
-guard({union, [{type, _, atom, _}|T]}, Name, Acc) ->
-    guard({union, T}, Name, Acc++"is_atom("++Name++")"++?OR(T));
-guard({union, [{type, _, binary, _}|T]}, Name, Acc) ->
-    guard({union, T}, Name, Acc++"is_binary("++Name++")"++?OR(T));
-guard({union, [{type, _, term, _}|T]}, Name, Acc) ->
-    guard({union, T}, Name, Acc++" true "++?OR(T));
-guard({union, [{type, _, integer, _}|T]}, Name, Acc) ->
-    guard({union, T}, Name, Acc++"is_integer("++Name++")"++?OR(T));
-guard({union, [{type, _, record, [{atom, _, R}]}|T]}, Name, Acc) ->
-    guard({union, T}, Name, Acc++"is_record("++Name++",'"++atom_to_list(R)++"')"++?OR(T));
-guard({union, [{type, _, list, T}]}, Name, Acc) ->
-    guard({list, T}, Name, Acc);
-guard({union, [{integer, _, V}|T]}, Name, Acc) ->
-    guard({union, T}, Name, Acc++Name++"=="++integer_to_list(V)++?OR(T)).
-
-head(L, []) -> L;
-head(L, H) -> [H|L].
-
-get_records({type,_,record,[{_,_,Class}]},Name) -> {atom_to_list(Class),"is_record("++Name++",'"++atom_to_list(Class)++"')"};
-get_records({type,_,binary,_},Name)             -> {[],"is_binary("++Name++")"};
-get_records({type,_,integer,_},Name)            -> {[],"is_integer("++Name++")"};
-get_records({type,_,nil,_},Name)                -> {[],Name++"==[]"};
-get_records({type,_,tuple,_},Name)              -> {[],"is_tuple("++Name++")"};
-get_records({type,_,list,_},Name)               -> {[],"is_list("++Name++")"};
-get_records({type,_,atom,_},Name)               -> {[],"is_atom("++Name++")"};
-get_records({atom,_,V},Name)                    -> {[],Name++"=='" ++ atom_to_list(V)++"'"};
-get_records(_,_)                                -> {[],[]}.
-
-get_fields(Name, Type) ->
-  Res = case Type of binary -> "<<_/binary>>"; _ -> u(Name) end,
-  lists:concat([Name, " = ", Res]).
+guard({list, [{type, I, N, R}]}, Name, Form, Acc) ->
+    guard({list, [{type, I, union, [{type, I, N, R}]}]}, Name, Form, Acc);
+guard({list, _}, Name, _Form, Acc) -> Acc++"is_list("++Name++") -> [];";
+guard({type, _, union, U}, Name, Form, Acc) -> guard({union, U}, Name, Form, Acc);
+guard({union, []}, _Name, _Form, Acc) -> Acc++[" -> Acc2;"];
+guard({union, [{user_type, _, U, _}|T]}, Name, Form = #form{types = Types}, Acc) ->
+    Type = element(2, lists:keyfind(U, 1, Types)),
+    guard({union, [Type|T]}, Name, Form, Acc);
+guard({union, [{atom, _, A}|T]}, Name, Form, Acc) ->
+    guard({union, T}, Name, Form, Acc++Name++"=='"++atom_to_list(A)++"'"++?OR(T));
+guard({union, [{type, _, union, _} = U|T]}, Name, Form, Acc) ->
+    guard({union, T}, Name, Form, lists:droplast(guard(U, Name, Form, Acc)) ++ ?OR(T));
+guard({union, [{type, _, nil, []}|T]}, Name, Form, Acc) ->
+    guard({union, T}, Name, Form, Acc++Name++"==[]"++?OR(T));
+guard({union, [{type, _, tuple, _}|T]}, Name, Form, Acc) ->
+    guard({union, T}, Name, Form, Acc++"is_tuple("++Name++")"++?OR(T));
+guard({union, [{type, _, iolist, _}|T]}, Name, Form, Acc) ->
+    guard({union, T}, Name, Form, Acc++"is_list("++Name++")"++?OR(T));
+guard({union, [{type, _, atom, _}|T]}, Name, Form, Acc) ->
+    guard({union, T}, Name, Form, Acc++"is_atom("++Name++")"++?OR(T));
+guard({union, [{type, _, binary, _}|T]}, Name, Form, Acc) ->
+    guard({union, T}, Name, Form, Acc++"is_binary("++Name++")"++?OR(T));
+guard({union, [{type, _, term, _}|T]}, Name, Form, Acc) ->
+    guard({union, T}, Name, Form, Acc++" true "++?OR(T));
+guard({union, [{type, _, integer, _}|T]}, Name, Form, Acc) ->
+    guard({union, T}, Name, Form, Acc++"is_integer("++Name++")"++?OR(T));
+guard({union, [{type, _, record, [{atom, _, R}]}|T]}, Name, Form, Acc) ->
+    guard({union, T}, Name, Form, Acc++"is_record("++Name++",'"++atom_to_list(R)++"')"++?OR(T));
+guard({union, [{type, _, list, T}]}, Name, Form, Acc) ->
+    guard({list, T}, Name, Form, Acc);
+guard({union, [{integer, _, V}|T]}, Name, Form, Acc) ->
+    guard({union, T}, Name, Form, Acc++Name++"=="++integer_to_list(V)++?OR(T)).
 
 prelude(Imports, Module) ->
     S = lists:flatten([io_lib:format("-include_lib(\"~s\").~n",[X])||X<-lists:usort(Imports)]),
