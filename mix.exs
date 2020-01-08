96ca1c6140d2ef07bcab064d6e900163347933c0
5HT <maxim@synrc.com>
2020-01-08 17:27:03 +0200

    3.1.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule RPC.Mixfile do
   def project() do
     [
       app: :rpc,
-      version: "0.9.2",
+      version: "3.1.1",
       elixir: "~> 1.7",
       description: "RPC Remote Procedure Call",
       package: package(),
